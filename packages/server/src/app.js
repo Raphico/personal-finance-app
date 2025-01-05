a8cba043b2c716669cdcd73d40bd52872230fc93
Raphico <raphicogit@gmail.com>
2025-01-06 00:05:44 +0100

    feat: add security and request handling middleware

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -1,5 +1,18 @@
 import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import { env } from "./env.js";
 
 export const app = express();
 
-app.get("/", (req, res) => res.end("hello, world"));
+app.use(helmet());
+app.use(
+  cors({
+    origin: env.CORS_ORIGIN,
+    methods: env.CORS_METHODS,
+    allowedHeaders: env.CORS_ALLOWED_HEADERS,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
--- packages/server/src/env.js
+++ packages/server/src/env.js
@@ -6,5 +6,16 @@ export const { data: env } = z
       .number({ coerce: true })
       .default(8080)
       .describe("port to listen for incoming http requests"),
+    CORS_ORIGIN: z
+      .string()
+      .describe("specifies the allowed origin(s) for cross-origin requests"),
+    CORS_METHODS: z
+      .string()
+      .default("*")
+      .describe("defines the allowed methods for cross-origin requests"),
+    CORS_ALLOWED_HEADERS: z
+      .string()
+      .default("*")
+      .describe("list all headers that are allowed in a cross-origin request"),
   })
   .safeParse(process.env);
