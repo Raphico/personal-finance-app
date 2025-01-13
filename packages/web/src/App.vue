2502f3d458fb698c494ad8d1539b1da3eb531d7b
Raphico <raphicogit@gmail.com>
2025-01-13 01:14:51 +0100

    feat(web): unhead

--- packages/web/src/App.vue
+++ packages/web/src/App.vue
@@ -1,24 +1,3 @@
-<script setup>
-import BaseButton from "./components/BaseButton.vue";
-import IconCaretRight from "./components/Icons/IconCaretRight.vue";
-</script>
-
 <template>
-  <div class="button-containers">
-    <BaseButton variant="primary">placeholder</BaseButton>
-    <BaseButton variant="secondary">placeholder</BaseButton>
-    <BaseButton variant="tertiary">
-      placeholder
-      <IconCaretRight />
-    </BaseButton>
-    <BaseButton variant="destroy">placeholder</BaseButton>
-  </div>
+  <RouterView />
 </template>
-
-<style scoped>
-.button-containers {
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-}
-</style>
---
+++ packages/web/src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useHead } from "@unhead/vue";
+
+useHead({
+  title: "Login - Personal Finance App",
+  meta: [
+    {
+      name: "description",
+      content: "Personal finance app login page",
+    },
+  ],
+});
+</script>
+
+<template>
+  <h1>Login</h1>
+</template>
