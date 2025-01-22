7013dc2a3b921c70b977207d2605bcdef678a1f5
Raphico <raphicogit@gmail.com>
2025-01-22 11:33:54 +0100

    refactor(server): verify email

--- packages/server/src/controllers/auth/verify-email.controller.js
+++ packages/server/src/controllers/auth/verify-email.controller.js
@@ -5,13 +5,7 @@ import { asyncHandler } from "../../utils/async-handler.js";
 import { users } from "../../db/schema.js";
 import { ApiResponse } from "../../utils/api-response.js";
 import { verifyEmailSchema } from "@repo/shared-validators/auth";
-import {
-  generateAccessToken,
-  generateHash,
-  generateRefreshToken,
-} from "../../services/token.service.js";
-import { env } from "../../config.js";
-import { redisClient } from "../../services/redis.service.js";
+import { generateHash } from "../../services/token.service.js";
 
 export const verifyEmail = asyncHandler(
   async function verifyEmail(request, response) {
@@ -59,34 +53,14 @@ export const verifyEmail = asyncHandler(
       })
       .where(eq(user.id, users.id));
 
-    const accessToken = generateAccessToken({
-      id: user.id,
-      name: user.name,
-      email: user.email,
-    });
-    const refreshToken = generateRefreshToken({
-      id: user.id,
-    });
-
-    const cookieOptions = {
-      secure: env.NODE_ENV == "production",
-      httpOnly: true,
-    };
-
-    await redisClient.set(`user:${user.id}:refreshToken`, refreshToken);
-
-    response
-      .status(200)
-      .cookie("refreshToken", refreshToken, cookieOptions)
-      .cookie("accessToken", accessToken, cookieOptions)
-      .json(
-        new ApiResponse({
-          data: {
-            isEmailVerified: true,
-          },
-          message: "email verification successful",
-          status: "ok",
-        })
-      );
+    response.status(200).json(
+      new ApiResponse({
+        data: {
+          isEmailVerified: true,
+        },
+        message: "email verification successful",
+        status: "ok",
+      })
+    );
   }
 );
