ca48e97310df155c2ac4c56ee8b72fb528e374a4
Raphico <raphicogit@gmail.com>
2025-02-06 22:20:12 +0100

    fix(web): lint errors

--- packages/web/src/components/BaseCheckbox.vue
+++ packages/web/src/components/BaseCheckbox.vue
@@ -3,6 +3,7 @@ const model = defineModel();
 </script>
 
 <template>
+  <!-- eslint-disable-next-line vuejs-accessibility/form-control-has-label -->
   <input type="checkbox" v-model="model" />
 </template>
 
